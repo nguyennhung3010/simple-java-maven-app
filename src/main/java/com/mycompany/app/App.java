50f2e6df78240b08c81edb5e17574f3efdc2146b
nguyennhung3010 <nguyennhung0579@gmail.com>
2024-10-17 10:52:44 +0700

    Update App 2.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World Savvy!";
+    private static final String MESSAGE = "Hello World Savvy hello!";
 
     public App() {}
 
